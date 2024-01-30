5ea7bd536c2723740578a357942bc1597592fa76
Peyton Williams <peytonwilliams5@gmail.com>
2024-01-29 23:03:30 -0500

    small additions

--- Scenes/Objects/hex.gd
+++ Scenes/Objects/hex.gd
@@ -1,17 +1,39 @@
 extends Node3D
 class_name Hex
 
+const BIOMES = {
+	"grass" : {
+		"color": Color.DARK_GREEN
+	},
+	"mountain" : {
+		"color": Color.SADDLE_BROWN
+	},
+	"desert" : {
+		"color": Color.SANDY_BROWN
+	}
+}
+
 @onready var gsr = GlobalStateReference
 
-@export var coords = Vector3i.ZERO
+@export var mesh : MeshInstance3D
 
+@export var coords = Vector3i.ZERO
 @export var player_owner := ""
-@export var improvement := ""
+
+var biome = "grass"
+var output = 5
+var improvement : Node3D = null
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	update_biome(biome)
 	pass # Replace with function body.
 
+func update_biome(new_biome):
+	biome = new_biome
+	var mat : StandardMaterial3D = mesh.mesh.surface_get_material(0)
+	mat.albedo_color = BIOMES[biome]["color"]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,8 +1,14 @@
 extends Node3D
 
+const RAY_LENGTH = 100
 const CAMERA_HORZ_SPEED := 20.0
+const COLLISION_MASK = 1
 
 @onready var gsr = GlobalStateReference
+
+@export var camera : Camera3D
+
+var raycast_result : Dictionary = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,3 +21,24 @@ func _process(delta):
 	if direction:
 		var new_position_flat = Vector2(position.x, position.z) + direction * delta * CAMERA_HORZ_SPEED
 		position = Vector3(new_position_flat.x, position.y, new_position_flat.y)
+		
+	if Input.is_action_just_pressed("input_click"):
+
+		if "collider" in raycast_result:
+			var hex_coords: Vector3i = raycast_result.collider.get_parent().coords
+			#select_hex.emit(hex_coords)
+			#if (player == 1):
+			#	click(hex_coords)
+			#else:
+			print("click coords:", hex_coords)
+
+func _physics_process(delta):
+	var mouse_pos = get_viewport().get_mouse_position()
+	var from = camera.project_ray_origin(mouse_pos)
+	var to = from + camera.project_ray_normal(mouse_pos) * RAY_LENGTH
+	var space = get_world_3d().direct_space_state
+	var ray_query = PhysicsRayQueryParameters3D.new()
+	ray_query.from = from
+	ray_query.to = to
+	ray_query.collision_mask = COLLISION_MASK
+	raycast_result = space.intersect_ray(ray_query)
